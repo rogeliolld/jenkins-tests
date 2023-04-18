c865f197ff9b31fb935f49ec66db1694ba9cf25e
rogeliolld <rogelio.llerenad@gmail.com>
2023-04-17 21:21:55 -0500

    Cambio #2

--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@ const sum = require('../');
 const assert = require('assert');
 
 describe('Sum', () => {
-  it('should handle positive numbers prueba 2', () => {
+  it('should handle positive numbers', () => {
     assert.equal(sum(3, 5), 8);
   });
   it('should handle negative numbers', () => {
